5a0455ea29b03c63986fdcb3a2a0493a7d324ae5
Hepsiba Selvi M <hepsi9361@gmail.com>
2026-06-01 15:38:40 +0530

    Updated config to use environment variables

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 class Config:
-    SECRET_KEY = 'your_secret_key_here_make_it_long_123456'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'root18'
-    MYSQL_DB = 'course_app'
-    MYSQL_PORT = 3306
-    JWT_SECRET_KEY = 'jwt_secret_key_here_make_it_long_123456'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key_here')
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'root123')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'course_app')
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt_secret_key_here')
